987d43a41dbc0e57b313b4111f01d571a7885c12
dongho5309 <dongho5309@naver.com>
2020-08-27 22:25:48 +0900

    Day1 - MemoryPool Pull in Project - Note Generator Main Function Compelete - FIle Read Gen Complete

---
+++ Velocity/Assets/Scripts/IDespawnable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// 디스폰 가능한 GameObject에 쓰이는 인터페이스 반환값 없는 이벤트 Action을 이용한 함수구현 
+/// </summary>
+public interface IDespawnable
+{
+    event Action<GameObject> OnDespawn;
+}
---
+++ Velocity/Assets/Scripts/NoteGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteGenerator : MonoBehaviour
+{
+    public string[] NoteData;
+    public Transform[] GeneratePos;
+    public GameObject NotePrefab;
+    Queue<string> timeLineQueue;
+    private void Awake()
+    {
+        NoteData = File.ReadAllLines(Application.streamingAssetsPath + "/test.txt");
+        timeLineQueue = new Queue<string>(NoteData.Length);
+        foreach (var note in NoteData)
+        {
+            //Debug.Log(note);
+            timeLineQueue.Enqueue(note);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        string[] data = timeLineQueue.Peek().Split("/".ToCharArray()[0]);
+
+        if (float.Parse(data[0]) <= Time.time)
+        {
+            for(int loopCount = 1; loopCount < data.Length; loopCount++)
+            {
+                Instantiate(NotePrefab, GeneratePos[int.Parse(data[loopCount])]);
+                Debug.Log($"Generate note at {int.Parse(data[loopCount])}, Time : {Time.time}");
+            }
+            timeLineQueue.Dequeue();
+        }
+        //Debug.Log(data[0]);
+        //Debug.Log(Time.time);
+        //if(data[0].Equals(Time.))
+    }
+}
