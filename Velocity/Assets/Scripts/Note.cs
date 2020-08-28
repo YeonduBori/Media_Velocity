4f5848ec3fce7d418a02bbde464a16e178a70386
dongho5309 <dongho5309@naver.com>
2020-08-28 10:38:47 +0900

    Folder Arrangement

--- Velocity/Assets/Scripts/Note.cs
+++ Velocity/Assets/Scripts/Note.cs
@@ -1,20 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Note : MonoBehaviour
+public class Note : MonoBehaviour, IDespawnable
 {
-
+    public int score;
     public float speed;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button button;
+    [SerializeField] Canvas canvas;
+    Image buttonImage;
+
+    public event Action<GameObject> OnDespawn;
+
+    void Awake()
     {
-        
+        canvas.worldCamera = Camera.main;
+        buttonImage = button.GetComponent<Image>();
+        OnDespawn += ResetStatus;
+    }
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        buttonImage.fillAmount = 0.0f;
+        StartCoroutine(RotateButton());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    public void Despawn()
+    {
+        StopCoroutine(RotateButton());
+        OnDespawn(gameObject);
+    }
+
+    void ResetStatus(GameObject gameObject)
+    {
+        buttonImage.fillAmount = 0.0f;
+    }
+
+    private IEnumerator RotateButton()
+    {
+        while (gameObject.activeSelf)
+        {
+            buttonImage.fillAmount += 0.002f;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
--- Velocity/Assets/Scripts/NoteGenerator.cs
+++ Velocity/Assets/Scripts/NoteGenerator.cs
@@ -8,9 +8,11 @@ public class NoteGenerator : MonoBehaviour
     public string[] NoteData;
     public Transform[] GeneratePos;
     public GameObject NotePrefab;
+    MemoryPool NotePool;
     Queue<string> timeLineQueue;
     private void Awake()
     {
+        NotePool = new MemoryPool(NotePrefab, 20, 50);
         NoteData = File.ReadAllLines(Application.streamingAssetsPath + "/test.txt");
         timeLineQueue = new Queue<string>(NoteData.Length);
         foreach (var note in NoteData)
@@ -33,7 +35,8 @@ public class NoteGenerator : MonoBehaviour
         {
             for(int loopCount = 1; loopCount < data.Length; loopCount++)
             {
-                Instantiate(NotePrefab, GeneratePos[int.Parse(data[loopCount])]);
+                NotePool.Respawn(GeneratePos[int.Parse(data[loopCount])].position, Quaternion.identity);
+                //Instantiate(NotePrefab, GeneratePos[int.Parse(data[loopCount])]);
                 Debug.Log($"Generate note at {int.Parse(data[loopCount])}, Time : {Time.time}");
             }
             timeLineQueue.Dequeue();
